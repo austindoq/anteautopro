a1659f622bd7c064ce6a371c95e532ebc3cb145f
Austin <austindoq@gmail.com>
2026-05-25 16:14:46 -0700

    Connect DB and express

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("Database Connected");
+}
+
+app.listen(process.env.PORT, () => {
+  console.log("Connection Live");
+  main();
+});
