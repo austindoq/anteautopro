c4e3c7dcb284eec09dcf63c7f0cf75112a9cf4c3
Austin <austindoq@gmail.com>
2026-06-17 11:01:21 -0700

    Fix footer background, fix unauthorized dashboard redirect

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,14 @@
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
+
+//Set relative paths to serve view from the backend
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const isAuthenticated = (req, res, next) => {
   if (req.session.isLoggedIn) {
     return next();
   } else {
-    res.redirect("/login.html");
+    return res.status(401).sendFile(join(__dirname, "../views/login.html"));
   }
 };
